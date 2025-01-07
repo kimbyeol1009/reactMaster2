89d5309a8def227ee17ec47287870e409e0e0e3c
byeol <kimbyul5633@gmail.com>
2025-01-07 14:26:03 +0900

    2025.01.07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import './App.css'
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import data from './data';
 function App() {
+
+  let [shoes] = useState(data);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div>
+        <Navbar bg="black" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="#home">React Store</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">products</Nav.Link>
+              <Nav.Link href="#pricing">items</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+      <div className="main-bg"></div>
+      <div className="image-gallery">
+        {
+          shoes.map(function(a, i) {
+            return (
+              <div key={i} className="image-item">
+                <Card data={shoes[i]} i={i+1} />
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+function Card(props){
+    return(
+      <div className="col-md-4">
+        <img src={'https://codingapple1.github.io/shop/shoes'+props.i+'.jpg'} width="80%" alt="description of the image" />
+        <h4>{props.data.title}</h4>
+        <p>{props.data.price}</p>
+      </div>
+    )
+  }
+
+export default App
