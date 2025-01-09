553ec3dc4abd7a77248ed39c7c6c5387dd7fe662
byeol <kimbyul5633@gmail.com>
2025-01-09 11:46:58 +0900

    2025.01.09

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import {Navbar, Container, Nav,} from 'react-bootstrap';
 import data from './data';
 import Detail from './routes/Detail';
+import Event from './routes/Event';
 import {Routes, Route, Link ,useNavigate, Outlet} from 'react-router-dom';
 
 
@@ -22,13 +23,14 @@ function App() {
             <Nav className="me-auto">
               <Link className="linkButton" to="/">홈</Link>
               <Link className="linkButton" to="/detail">Detail</Link>
+              
               {/* 
               navigate함수임! : 페이지를 이동시켜준다
               <button onClick={()=>{navigate('/detail')}} className="linkButton">Detail</button> */}
             </Nav>
           </Container>
         </Navbar>
-
+        <Link to = "/event"></Link>
 {/* ------라우터------ */}
 
         <Routes>
@@ -49,6 +51,16 @@ function App() {
             </div>
             }/>
           <Route path="/detail" element={<Detail/>}/>
+          {/* Nested Routes : 태그안에 태그가 들어간 라우터 형식, /about/member, /about/location 을 뜻함 */}
+          <Route path="/about" element={<About/>}>
+            <Route path ="member" element={<div>멤버임</div>}/>
+            <Route path ="location" element={<div></div>}/>
+          </Route>
+          <Route path="/event" element={<Event/>}>
+            <Route path = "one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
+            <Route path = "two" element={<div>생일기념 쿠폰받기</div>}></Route>
+          
+          </Route>
 
         </Routes>
 
@@ -59,6 +71,18 @@ function App() {
 }
 
 {/* ------Card컴포넌트------ */}
+function About(){
+  return(
+    <div>
+      <h4>회사정보임</h4>
+      {/* Outlet : 구멍이라는 뜻임, Nested Routes된 애들을 어디에 보여줄지를 결정해줌 */}
+      <Outlet></Outlet>
+    </div>
+
+  )
+}
+
+
 
 function Card(props){
     return(
